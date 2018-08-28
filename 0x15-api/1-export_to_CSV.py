a4824ea6cf17ce16afe2249d5e7698ba210edcc5
arturadamian <arturadamian@gmail.com>
2018-08-28 06:49:16 +0000

    username

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-export data in the JSON format
+export data in the CSV format
 """
 import csv
 import requests
@@ -12,14 +12,12 @@ if __name__ == "__main__":
     Records all tasks that are owned by this employee
     """
     Id = int(argv[1])
-    emp_name = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'
-        .format(Id)).json().get('name')
-    all_tasks = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'
-        .format(Id)).json()
+    emp_name = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                            .format(Id)).json().get("username")
+    all_tasks = requests.get("https://jsonplaceholder.typicode.com/"
+                             "todos?userId={}".format(Id)).json()
     with open("{}.csv".format(Id), "w") as fd:
-        fill = csv.writer(fd, delimiter=',', quoting=csv.QUOTE_ALL)
+        fill = csv.writer(fd, quoting=csv.QUOTE_ALL)
         for task in all_tasks:
             fill.writerow([Id, emp_name,
-                           task['completed'], task['title']])
+                           task.get("completed"), task.get("title")])
