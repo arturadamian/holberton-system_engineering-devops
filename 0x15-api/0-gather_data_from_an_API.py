b1c68251c4d0672264a75ee7ec389ecf32473e58
arturadamian <arturadamian@gmail.com>
2018-08-28 05:43:51 +0000

    dont use func name

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,23 +6,21 @@ returns information about his/her TODO list progress
 import requests
 from sys import argv
 
-def get_employee():
+
+if __name__ == "__main__":
     """prints the requested info"""
 
+    Id = int(argv[1])
     employee = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}'
-        .format(argv[1])).json()
+        .format(Id)).json()
     all_tasks = requests.get(
         'https://jsonplaceholder.typicode.com/todos?userId={}'
-        .format(argv[1])).json()
+        .format(Id)).json()
     done_tasks = requests.get(
         'https://jsonplaceholder.typicode.com/todos?'
-        'userId={}&&completed=true'.format(argv[1])).json()
-    try:
-        print("Employee {} is done with tasks({}/{}):".format(
-            employee['name'], len(done_tasks), len(all_tasks)))
-        [print("\t {}".format(task['title'])) for task in done_tasks]
-    except:
-        pass
-if __name__ == "__main__":
-    get_employee()
+        'userId={}&&completed=true'.format(Id)).json()
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee['name'], len(done_tasks), len(all_tasks)))
+    [print("\t {}".format(task['title'])) for task in done_tasks]
