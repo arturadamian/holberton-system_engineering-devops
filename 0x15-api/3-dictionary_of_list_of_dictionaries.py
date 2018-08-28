d2c8731996bcba2f7df0b0c10d03dd1646f70c56
arturadamian <arturadamian@gmail.com>
2018-08-28 20:22:37 +0000

    Records all tasks from all employees in json

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+export data in the JSON format
+"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+    Records all tasks from all employees
+    """
+    js_dct = {}
+    js_lst = []
+
+    emp = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    for item in emp:
+        Id = item['id']
+        all_tasks = requests.get("https://jsonplaceholder.typicode.com/"
+                                 "todos?userId={}".format(Id)).json()
+        task_dct = {}
+        task_dct['username'] = item['name']
+        for task in all_tasks:
+            task_dct['completed'] = task['completed']
+            task_dct["task"] = task["title"]
+        js_lst.append(task_dct)
+        js_dct[Id] = js_lst
+
+    with open("todo_all_employees.json", 'w') as jsf:
+        json.dump(js_dct, jsf)
